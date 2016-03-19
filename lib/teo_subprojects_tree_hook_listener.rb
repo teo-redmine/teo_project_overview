9e226cbe2c00d22f7aa312b989202958214da805
Juan Leal <juan.leal@juntadeandalucia.es>
2016-03-19 17:45:22 +0100

    Estilos ajustados

--- lib/teo_subprojects_tree_hook_listener.rb
+++ lib/teo_subprojects_tree_hook_listener.rb
@@ -1,5 +1,19 @@
 class TeoSubprojectsTreeHookListener < Redmine::Hook::ViewListener
   render_on :view_projects_show_right, :partial => "projects/subtree"
-#  render_on :view_projects_show_left, :partial => "wiki/content"
+  #render_on :view_projects_show_left, :partial => "wiki/content", :locals => {:content => Project.find('si').wiki.find_page(nil).content_for_version(nil)}
+  def view_projects_show_left(context = {})
+    wiki = Project.find(context[:project]).wiki
+    page = wiki.find_page(nil) if wiki
+    content = page.content_for_version(nil) if page
+    if content
+      context[:content] = content
+      return context[:controller].send(:render_to_string, {
+          :partial => 'wiki/content',
+          :locals => context
+      })
+    else
+      return content_tag("p", "Cree una wiki de proyecto para que aparezca aquí.")
+    end
+  end
 end
 
