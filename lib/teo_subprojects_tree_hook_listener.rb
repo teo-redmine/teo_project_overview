5cabd63084a53885ab3c3589a1b34ee5a7f72a61
Juan Leal <juan.leal@juntadeandalucia.es>
2016-03-31 07:36:27 +0200

    Cambiada la forma de modificar views/projects/show

--- lib/teo_subprojects_tree_hook_listener.rb
+++ lib/teo_subprojects_tree_hook_listener.rb
@@ -1,7 +1,7 @@
 class TeoSubprojectsTreeHookListener < Redmine::Hook::ViewListener
-  render_on :view_projects_show_right, :partial => "projects/subtree"
+  render_on :view_projects_show_right_teo, :partial => "projects/subtree"
 
-  def view_projects_show_left(context = {})
+  def view_projects_show_left_teo(context = {})
     wiki = Project.find(context[:project]).wiki
     page = wiki.find_page(nil) if wiki
     content = page.content_for_version(nil) if page
@@ -16,4 +16,3 @@ class TeoSubprojectsTreeHookListener < Redmine::Hook::ViewListener
     end
   end
 end
-
