a0890000bf08827637656260c678d15b1189d2b0
Juan Leal <juan.leal@juntadeandalucia.es>
2016-04-03 20:50:00 +0200

    Apartado para subproyectos con h3 y wiki sólo si módulo activado

--- lib/teo_subprojects_tree_hook_listener.rb
+++ lib/teo_subprojects_tree_hook_listener.rb
@@ -2,17 +2,16 @@ class TeoSubprojectsTreeHookListener < Redmine::Hook::ViewListener
   render_on :view_projects_show_right_teo, :partial => "projects/subtree"
 
   def view_projects_show_left_teo(context = {})
-    wiki = Project.find(context[:project]).wiki
+    p = Project.find(context[:project])
+    wiki = p.wiki
     page = wiki.find_page(nil) if wiki
     content = page.content_for_version(nil) if page
-    if content
+    if p.enabled_module(:wiki) && content
       context[:content] = content
       return context[:controller].send(:render_to_string, {
           :partial => 'wiki/content',
           :locals => context
       })
-    else
-      return content_tag("p", l(:create_wiki_message))
     end
   end
 end
