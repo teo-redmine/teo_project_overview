f568ff394e2ab4b75072f011d78fac57e8863543
Cosmin Puiu Ionut <cosminpuiu@guadaltel.com>
2018-11-13 15:52:20 +0100

    update plugin to support Redmine 3.4.x

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :teo_project_overview do
   name 'Teo Project Overview plugin'
   author 'Junta de Andalucía'
   description 'Replaces project overview pages'
-  version '1.0.0'
+  version '1.0.1'
   url 'https://github.com/teo-redmine'
   author_url 'http://www.juntadeandalucia.es'
 end
